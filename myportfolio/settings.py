9256e89e694e49216b95f1c0557732f80b5f9c85
Puskar Joshi <puskarjoshi22@gmail.com>
2020-08-27 11:48:27 +0545

    added procfile and requirements

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
