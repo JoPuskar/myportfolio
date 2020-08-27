9b1423d50bfddf378306af4d4dad5846a635becd
Puskar Joshi <puskarjoshi22@gmail.com>
2020-08-27 15:31:44 +0545

    whitenoise disabled

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
